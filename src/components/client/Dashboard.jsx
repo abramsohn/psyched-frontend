9351871bd6a538850526c9dc17288d8c962cfb07
Ido Abramsohn <ido.abramsohn@gmail.com>
2021-04-08 16:44:09 -0400

    move greeting from sidebar to dashboard

--- src/components/client/Dashboard.jsx
+++ src/components/client/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useAuth } from '../../useAuth.js'
 
 const Dashboard = () => {
-
+     let auth = useAuth();
+    
+    function generateGreeting() {
+        const currentTime = new Date().getHours()
+        if (currentTime >= 6 && currentTime < 12) return 'Good Morning';
+        else if (currentTime >= 12 && currentTime < 18) return 'Good Afternoon';
+        else if (currentTime >= 18 && currentTime < 21) return 'Good Evening';
+        else if (currentTime >= 21 || currentTime < 6) return 'Good Night';
+        
+    }
     return (
-        <h1>Dashboard</h1>
+        <h1><span className="greeting">{generateGreeting()}  {auth.user} </span></h1>
     )
 }
 
--- src/components/client/Sidebar.jsx
+++ src/components/client/Sidebar.jsx
@@ -1,17 +1,7 @@
 import React from 'react'
-import { useAuth } from '../../useAuth.js'
 import { Link } from 'react-router-dom'
 function Sidebar() {
-    let auth = useAuth();
-    
-    function generateGreeting() {
-        const currentTime = new Date().getHours()
-        if (currentTime >= 6 && currentTime < 12) return 'Good Morning';
-        else if (currentTime >= 12 && currentTime < 18) return 'Good Afternoon';
-        else if (currentTime >= 18 && currentTime < 21) return 'Good Evening';
-        else if (currentTime >= 21 || currentTime < 6) return 'Good Night';
-        
-    }
+   
     return (
         <>
             <header id="user">
@@ -19,7 +9,6 @@ function Sidebar() {
                     <img src="https://www.fillmurray.com/300/300" alt="avatar"/>
                 </div>
                 <div className="user-info">
-                    <span className="greeting">{generateGreeting()}  {auth.user} </span>
                     Edit Profile
                 </div>
             </header>
